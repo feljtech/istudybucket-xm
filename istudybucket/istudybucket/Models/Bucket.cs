e872f9286ccf1f117a41cb84f2a4ab7e14323d81
Elroy Kanye <elroykanye@gmail.com>
2021-09-14 06:50:26 +0100

    initial commit

---
+++ istudybucket/istudybucket/Models/Bucket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace istudybucket.Models
+{
+    public class Bucket
+    {
+        private string BucketTitle { get; set; }
+        private string Description { get; set; }
+        private string CreatorName { get; set; }
+        private long Participants { get; set; }
+        private string CreationDate { get; set; }
+    }
+}
---
+++ istudybucket/istudybucket/Pages/Auth/RegisterPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using istudybucket.Forms;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace istudybucket.Pages.Auth
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_OnClicked(object sender, EventArgs e)
+        {
+            RegisterForm registerForm = new RegisterForm(UsernameEntry.Text, EmailEntry.Text, PasswordEntry.Text);
+            DisplayAlert("Reg info", registerForm.Email + registerForm.Password + registerForm.Username, "Ok");
+            // throw new NotImplementedException();
+        }
+    }
+}
